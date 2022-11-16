8511e79f48c748c3b5fb375620a56998728ffa90
Vladislav-Poberezhets <vladpoberezhets1@gmail.com>
2022-11-16 21:20:15 +0200

    Add project files.

---
+++ Voting/Ballot.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Voting
+{
+    internal class Ballot
+    {
+        private int[] CompanyList = new int[4] { 0, 0, 0, 0 };
+        private string[] CompanyNameList = new string[4] { "Apple", "Huawei", "Samsung", "Nokia" };
+
+        public bool VoteAdd(int indexCompany)
+        {
+            if (indexCompany > 4 || indexCompany < 1)
+                return false;
+            CompanyList[--indexCompany]++;
+            return true;
+        }
+
+        public void PrintNameList()
+        {
+            int i = 1;
+            foreach (string s in CompanyNameList)
+                Console.WriteLine((i++).ToString() + ". " + s);
+        }
+
+        public void PrintResult()
+        {
+            Console.WriteLine("Voting result:");
+            for (int i = 0; i < 4; i++)
+                Console.WriteLine(CompanyNameList[i] + " - " + CompanyList[i]);
+        }
+    }
+}
---
+++ Voting/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Voting
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Select who you want to vote for.\nPress 0 to display the voting results");
+            Ballot b = new Ballot(); b.PrintNameList();
+            int i = -1;
+            while (i != 0)
+            {
+                i = int.Parse(Console.ReadLine());
+                if (b.VoteAdd(i)) 
+                    Console.WriteLine("Your choice is recorded");
+            }
+            b.PrintResult();
+            
+            
+        }
+    }
+
+    
+}
